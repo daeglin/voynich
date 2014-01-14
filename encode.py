1d0aa306c965121e9d0b8206c61952d0ec02bd47
Michal Benes <michal.benes@wonderland.cz>
2014-01-14 09:08:41 -0500

    Special version

--- encode.py
+++ encode.py
@@ -7,43 +7,39 @@
 #
 # History:
 # January 12th 2014 - First version
-# January 14th 2014 - Working version
+# January 14th 2014 - Special version
 
 import sys
 
 line = sys.stdin.readline().strip()
 
-CUT = 10  # >= 8
-alphabet = "EITAUSRNMOCDLPQBVHGFXJYZ"
-alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet = "EITAUSRNMOCDLPQBVHGFXJYZKW"
+alphabetex = alphabet + alphabet.lower()
 
-value = {}
-base = 0
-for i in alphabet:
-    mult = 1
-    if i >= CUT:
-        mult = 2
-    value[i] = (base, mult)
-    base += mult
 
-state = 0
-mult = 1
+leftrule = {}
+n = 0
+for i in alphabetex:
+    for j in alphabet:
+        leftrule[i + j] = n
+        n += 1
+
+rightrule = []
+for i in range(len(alphabetex)):
+    for j in range(len(alphabetex)-i):
+        rightrule.append(alphabetex[i] + alphabetex[j])
+
+for i in range(len(alphabet)):
+    for j in range(len(alphabet)):
+        rightrule.append(alphabet[-1] + alphabet[j])
+
+s = "A"
 res = ""
 for a in line:
-    v = value[a]
-    state = state * base * mult + v[0] * mult
-    c = state / mult
-    res += alphabet2[c]
-    state = state - c * mult
+    idx = leftrule[s+a]
+    res += rightrule[idx][0]
+    s = rightrule[idx][1]
 
-    mult *= v[1]
-
-    while mult > 1 and state % 2 == 0:
-        mult /= 2
-        state /= 2
-
-    if mult > 1024 * 1024:
-        mult /= 2
-        res += "1"
-
+res += s
 print res
